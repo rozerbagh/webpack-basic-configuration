88c495c1d36ff9942f16c1c58908849778dab59b
rozerbagh <40331263+rozerbagh@users.noreply.github.com>
2020-07-04 08:59:45 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/js/index.js',
+        vendor: './src/js/vendor.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'images'
+                    }
+                }
+            }
+        ]
+    }
+};
